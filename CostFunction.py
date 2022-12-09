acac37b0d9095c16e74db7904c8142fce18e4dfa
ShivaTejaHitam <61039007+ShivaTejaHitam@users.noreply.github.com>
2022-12-09 11:50:59 +0530

    Update CostFunction.py

--- CostFunction.py
+++ CostFunction.py
@@ -1,4 +1,11 @@
 #Implementation of cost function
 
 def computeCost(X,y,theta):
-  return 0
+  k = (1 // (2 * m))
+  theta0 = 5
+  theta1 = 1
+  total = 0
+  for i in range(m):
+    total = total + (((theta0 + theta1*X[i])-y[i])**2)
+  return k* total
+    
